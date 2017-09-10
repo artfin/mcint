053f9883c62b4f7bf33e0b864cb6d5f491f7c173
artfin <artfin@mail.ru>
2017-09-10 20:47:05 +0300

    added danila exact pictures for co2ar

--- MCMC/out/plot_co2ar.py
+++ MCMC/out/plot_co2ar.py
@@ -1,30 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_file( filename ):
+def read_file( filename, n ):
     with open(filename, mode = 'r') as inputfile:
         lines = inputfile.readlines()
 
-    theta = []
-    pR = []
-    pT = []
-    jx = []
-    jy = []
-    jz = []
+    lists = [ [] for _ in range(n) ]
 
     for line in lines:
         if '#' in line:
             continue
         
         data = line.split()
-        theta.append( float(data[0]) )
-        pR.append( float(data[1]) )
-        pT.append( float(data[2]) )
-        jx.append( float(data[3]) )
-        jy.append( float(data[4]) )
-        jz.append( float(data[5]) )
+        for i in range(len(data)):
+            lists[i].append( float(data[i]) )
 
-    return theta, pR, pT, jx, jy, jz
+    return lists 
 
 def transform_theta( theta ):
     theta_i = []
@@ -43,14 +34,16 @@ def transform_theta( theta ):
     
     return theta_i
 
-theta, pR, pT, jx, jy, jz = read_file( 'co2ar.txt' )
+theta, pR, pT, jx, jy, jz = read_file( 'co2ar.txt', n = 6 )
+pr_d, pt_d, jx_d, jy_d, jz_d = read_file( 'distribution_arco2_danila.txt', n = 5 )
 # theta = transform_theta( theta )
 
 fig, ax = plt.subplots( figsize=[8, 6] )
 
-ax.set_title("Jz distribution")
+ax.set_title("pR distribution")
 
-n_mh, bins_mh, patches_mh = ax.hist( jz, bins = 100, normed = True, color = '#777777')
+n_mh, bins_mh, patches_mh = ax.hist( pR, bins = 100, normed = True, color = '#777777')
+n_d, bins_d, patches_d = ax.hist( pr_d, bins = 100, normed = True, color = 'red' )
 
 plt.grid( linestyle = ':', alpha = 0.7)
 plt.show()
